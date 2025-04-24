c51f856f0d06304d5c92c7f8a57dc613e6d37d91
meliosu <alex.atyurin@gmail.com>
2025-04-24 20:53:25 +0700

    add running migrations

--- src/database.rs
+++ src/database.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use sqlx::PgPool;
+use sqlx::{migrate::MigrateDatabase, PgPool};
 
 #[derive(Clone)]
 pub(crate) struct Database {
@@ -9,7 +9,17 @@ pub(crate) struct Database {
 
 impl Database {
     pub async fn new(postgres_url: &str) -> sqlx::Result<Self> {
+        // Create database if it doesn't exist
+        if !sqlx::Postgres::database_exists(postgres_url).await? {
+            sqlx::Postgres::create_database(postgres_url).await?;
+        }
+        
         let pool = PgPool::connect(postgres_url).await?;
+        
+        // Run migrations
+        sqlx::migrate!("./migrations")
+            .run(&pool)
+            .await?;
 
         Ok(Self {
             pool: Arc::new(pool),
